2ed85879e856abf84368b373f9a9c19995209177
K.S.KIM <nullable@kakao.com>
2020-07-13 14:01:47 +0900

    chore: spring project 생성 및 의존성 추가

---
+++ backend/src/main/java/com/woowacourse/taggle/TaggleApplication.java
@@ -0,0 +1,13 @@
+package com.woowacourse.taggle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaggleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaggleApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/woowacourse/taggle/TaggleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woowacourse.taggle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaggleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
