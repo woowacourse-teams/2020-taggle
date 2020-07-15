b593b325581f63514b80c03c93fa605387353c00
SeYun <ksy90101@naver.com>
2020-07-15 17:24:28 +0900

    feat(security): 스프링 시큐리티 설정 적용

--- backend/web/src/main/java/com/woowacourse/taggle/WebApplication.java
+++ backend/web/src/main/java/com/woowacourse/taggle/WebApplication.java
@@ -2,8 +2,10 @@ package com.woowacourse.taggle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application-oauth.properties")
 public class WebApplication {
     public static void main(final String[] args) {
         SpringApplication.run(WebApplication.class, args);
--- backend/web/src/main/java/com/woowacourse/taggle/security/config/WebSecurityConfig.java
+++ backend/web/src/main/java/com/woowacourse/taggle/security/config/WebSecurityConfig.java
@@ -1,11 +1,33 @@
 package com.woowacourse.taggle.security.config;
 
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import com.woowacourse.taggle.security.service.CustomOAuth2UserService;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable();
+
+        http
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService);
+    }
 }
