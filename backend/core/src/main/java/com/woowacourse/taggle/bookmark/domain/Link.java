f95e186b92b7dca338cf0f0a8092e8277968cfcd
K.S.KIM <nullable@kakao.com>
2020-07-15 14:47:22 +0900

    feat(bookmark): Link id 필드 제거
    * Embeddable로 변경 예정

--- backend/core/src/main/java/com/woowacourse/taggle/bookmark/domain/Link.java
+++ backend/core/src/main/java/com/woowacourse/taggle/bookmark/domain/Link.java
@@ -3,12 +3,6 @@ package com.woowacourse.taggle.bookmark.domain;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
 import com.woowacourse.taggle.bookmark.exception.UrlFormatMismatchException;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -16,24 +10,18 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-@Entity
 public class Link {
     private static final Pattern URL_PATTERN = Pattern.compile(
             "(^(https?|ftp|file)://)?[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]");
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(nullable = false)
     private String url;
 
-    public Link(String url) {
+    public Link(final String url) {
         validate(url);
         this.url = url;
     }
 
-    private void validate(String url) {
+    private void validate(final String url) {
         Matcher matcher = URL_PATTERN.matcher(url);
         if (!matcher.matches()) {
             throw new UrlFormatMismatchException("url이 올바르지 않습니다.\n"
