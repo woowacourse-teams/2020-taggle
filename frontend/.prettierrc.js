7ef67b3af8f2bab59f45096a5c135b555eb6169d
SeYun <ksy90101@naver.com>
2020-07-23 14:05:43 +0900

    chore(front-end) prettier custom 설정

---
+++ frontend/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  endOfLine: "lf",
+  trailingComma: false,
+  bracketSpacing: true,
+  printWidth: 150,
+  jsxBracketSameLine: false,
+};
