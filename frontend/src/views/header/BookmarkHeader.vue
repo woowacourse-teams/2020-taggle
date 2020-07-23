b71a8a003cd5f6567c78bd12793371d79984b004
SeYun <ksy90101@naver.com>
2020-07-23 17:35:04 +0900

    feat(tag-list) : 태그 리스트 컴포넌트 구현

--- frontend/src/views/header/BookmarkHeader.vue
+++ frontend/src/views/header/BookmarkHeader.vue
@@ -1,49 +1,50 @@
 <template>
   <div>
-    <v-navigation-drawer v-model="drawer" :mini-variant.sync="mini" permanent>
-      <v-list-item class="px-2">
-        <v-list-item-avatar>
-          <v-img src="@/assets/images/TaggleLog.png"></v-img>
-        </v-list-item-avatar>
-        <v-btn icon @click.stop="mini = !mini">
-          <v-icon>mdi-chevron-left</v-icon>
-        </v-btn>
+    <v-navigation-drawer permanent app mini-variant>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            <v-img src="@/assets/images/TaggleLog.png"></v-img>
+          </v-list-item-title>
+        </v-list-item-content>
       </v-list-item>
+
       <v-divider></v-divider>
-      <v-list dense>
-        <v-list-item v-for="item in items" :key="item.title" link>
+
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" link @click="toggleBookmarkList">
           <v-list-item-icon>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-icon>
+
           <v-list-item-content>
             <v-list-item-title>{{ item.title }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <TagList />
+    <TagList v-show="isBookmarkList" />
   </div>
 </template>
+
 <script>
-import TagList from './components/BookmarkList'
+import TagList from './components/TagList'
 
 export default {
+  name: 'BookmarkHeader',
   components: {
     TagList
   },
-
   data() {
     return {
-      drawer: true,
-      items: [
-        {
-          title: '',
-          icon: 'local_offer'
-        }
-      ],
-      mini: true
+      items: [{ title: 'tags', icon: 'local_offer' }],
+      isBookmarkList: false
+    }
+  },
+  methods: {
+    toggleBookmarkList: function() {
+      this.isBookmarkList = !this.isBookmarkList
     }
   }
 }
 </script>
-<style></style>
---
+++ frontend/src/views/header/components/TagList.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-card width="250" tile id="container">
+    <v-list>
+      <v-list-group value="true" v-for="(tagValue, i) in tagList" :key="i" class="text-center">
+        <template v-slot:activator>
+          <v-list-item-title>{{ tagValue.title }}</v-list-item-title>
+        </template>
+
+        <v-list-item v-for="(tag, i) in tagValue.tags" :key="i" link>
+          <v-list-item-icon>
+            <v-chip class="ma-auto" color="pink" label text-color="white">
+              <v-icon left>mdi-label</v-icon>
+              {{ tag }}
+            </v-chip>
+          </v-list-item-icon>
+        </v-list-item>
+      </v-list-group>
+    </v-list>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'TagList',
+  data() {
+    return {
+      tagList: [
+        {
+          title: '백엔드',
+          tags: ['#자바', '#노드', '#스프링']
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+#container {
+  position: relative;
+  left: 50px;
+  top: 50px;
+}
+</style>
