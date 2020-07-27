dbca06680307fa0368f695726d55cb614a3551fa
SeYun <ksy90101@naver.com>
2020-07-27 20:57:25 +0900

    chore(front-end): vue-plugin-eslint, airbnb eslint 적용
    - 세미콜론
    - 콤마
    - new 연산자
    - indent
    - import 확장자

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -3,12 +3,16 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: ["plugin:vue/essential", "airbnb-base", "@vue/prettier"],
   parserOptions: {
     parser: "babel-eslint"
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    semi: ['error','always'],
+    'comma-dangle': 'off',
+    'no-new': 'off',
+    indent: ['error', 2],
+    'import/extensions': 'error',
+    'import/no-unresolved': 'off'
   }
 };
