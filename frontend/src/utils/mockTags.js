1019cf8a0ab65eaa7c7189f47e5e1772495e1440
김준석(티거) <jnunseok@gmail.com>
2020-07-28 18:01:00 +0900

    feat(frontend): 북마크 관련 개인 페이지 구현 (#51)
    * style(backend): indent 4 space 변경
    
    * chore(front-end): vuetify init 및 경로 설정
    
    * chore(front-end) prettier custom 설정
    
    * feature(bookmark-page): Page 만들기
    
    * refactor(bookmark-page): component 분리
    
    * feat(bookmark-page): 헤더 및 태그 리스트 컴포넌트 생성
    
    * feat(tag-list) : 태그 리스트 컴포넌트 구현
    
    * feat(tag-list) : 태그 리스트 toggle 형태에서 고정으로 변경
    
    * refactor(bookmark-page) : bookmarkCard 위치 수정
    
    * refactor(Tag-List): 카드 상단에 고정
    
    * refactor(tag-list) : z-index로 tag-list 상위로 이동
    
    * refactor(bookmark-page): mock data 수정 및 컨벤션 적용
    
    * refactor(tagList): mock 데이터 분리
    
    * refactor(tagList): mock 데이터에 id 추가
    
    * refactor(tagList): 태그 리스트 디자인 변경
    
    * refactor(bookmark-page): 전반적인 수정
    
    - merge된 pr 가져오기
    - 컨벤션 적용
    - 북마크 크기 조정
    
    * refactor(bookmark-page): 카드 크기 고정 및 레이아웃 수정
    
    * refactor(bookmark-page): 카드 flex 수정 및 레이아웃 중앙 정렬
    
    * refactor(bookmark-page): 카드 flex 수정, 레이아웃 중앙 정렬, 불필요한 data() 제거
    
    * refactor(bookmark-page): 불필요한 css 제거
    
    Co-authored-by: SeYun <ksy90101@naver.com>
    Co-authored-by: KS-KIM <nullable@kakao.com>

---
+++ frontend/src/utils/mockTags.js
@@ -0,0 +1,67 @@
+export const categories = [
+  {
+    id: 1,
+    title: '백엔드',
+    tags: [
+      { id: 1, name: '자바' },
+      { id: 2, name: '노드' },
+      { id: 3, name: '스프링' },
+    ],
+  },
+  {
+    id: 2,
+    title: '프론트엔드',
+    tags: [
+      { id: 1, name: '자바스크립트' },
+      { id: 2, name: 'jun' },
+      { id: 3, name: 'vue.js' },
+    ],
+  },
+  {
+    id: 3,
+    title: 'UI/UX',
+    tags: [
+      { id: 1, name: '디자인' },
+      { id: 2, name: '로고' },
+      { id: 3, name: '배열정리' },
+    ],
+  },
+];
+
+export const bookmarks = [
+  {
+    img: 'https://t1.daumcdn.net/cfile/tistory/212EFE4D58D72EB70D',
+    title: 'Test Case1',
+    tags: [{ name: 'Java' }, { name: 'Spring' }, { name: 'Vue' }, { name: 'Vuetify' }],
+  },
+  {
+    img: 'https://t1.daumcdn.net/cfile/tistory/212EFE4D58D72EB70D',
+    title: 'Test Case1',
+    tags: [{ name: 'Java' }, { name: 'Spring' }, { name: 'Vue' }],
+  },
+  {
+    img: 'https://t1.daumcdn.net/cfile/tistory/212EFE4D58D72EB70D',
+    title: 'Test Case1',
+    tags: [{ name: 'Java' }, { name: 'Spring' }, { name: 'Vue' }],
+  },
+  {
+    img: 'https://t1.daumcdn.net/cfile/tistory/212EFE4D58D72EB70D',
+    title: 'Test Case1',
+    tags: [{ name: 'Java' }, { name: 'Spring' }, { name: 'Vue' }],
+  },
+  {
+    img: 'https://t1.daumcdn.net/cfile/tistory/212EFE4D58D72EB70D',
+    title: 'Test Case1',
+    tags: [{ name: 'Java' }, { name: 'Spring' }, { name: 'Vue' }],
+  },
+  {
+    img: 'https://t1.daumcdn.net/cfile/tistory/212EFE4D58D72EB70D',
+    title: 'Test Case1',
+    tags: [{ name: 'Java' }, { name: 'Spring' }, { name: 'Vue' }],
+  },
+  {
+    img: 'https://t1.daumcdn.net/cfile/tistory/212EFE4D58D72EB70D',
+    title: 'Test Case1',
+    tags: [{ name: 'Java' }, { name: 'Spring' }, { name: 'Vue' }],
+  },
+];
