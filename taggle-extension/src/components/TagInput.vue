b4e8599b37676f2a703f71c7907eedc5e8e090a0
SeYun <ksy90101@naver.com>
2020-07-28 10:54:12 +0900

    chore(chrome-extension): eslint 및 prettier 설정

--- taggle-extension/src/components/TagInput.vue
+++ taggle-extension/src/components/TagInput.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="mt-1">
-    <vue-tags-input v-model="tag" :tags="tags" @tags-changed="newTags => (tags = newTags)" placeholder="태그 추가" />
+    <vue-tags-input v-model="tag" :tags="tags" @tags-changed="(newTags) => (tags = newTags)" placeholder="태그 추가" />
   </div>
 </template>
 
