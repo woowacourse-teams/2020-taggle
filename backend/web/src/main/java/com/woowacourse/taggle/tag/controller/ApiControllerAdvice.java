c7f9ad4a64c5b6d484201715fa1de61b9fbd53d5
K.S.KIM <nullable@kakao.com>
2020-07-31 01:20:54 +0900

    refactor(backend): 예외 케이스 처리 및 로깅 설정

--- backend/web/src/main/java/com/woowacourse/taggle/tag/controller/ApiControllerAdvice.java
+++ backend/web/src/main/java/com/woowacourse/taggle/tag/controller/ApiControllerAdvice.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.woowacourse.taggle.tag.exception.BookmarkNotFoundException;
+import com.woowacourse.taggle.tag.exception.TagBookmarkNotFoundException;
 import com.woowacourse.taggle.tag.exception.TagNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 
@@ -18,8 +19,8 @@ public class ApiControllerAdvice {
         log.error(exception.getMessage());
 
         return ResponseEntity
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(exception.getMessage());
+                .status(HttpStatus.NOT_FOUND)
+                .body("태그를 찾을 수 없습니다.");
     }
 
     @ExceptionHandler(BookmarkNotFoundException.class)
@@ -27,7 +28,25 @@ public class ApiControllerAdvice {
         log.error(exception.getMessage());
 
         return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body("북마크를 찾을 수 없습니다.");
+    }
+
+    @ExceptionHandler(TagBookmarkNotFoundException.class)
+    public ResponseEntity<String> handleTagBookmarkNotFoundException(Exception exception) {
+        log.error(exception.getMessage());
+
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body("태그에 속한 북마크를 찾을 수 없습니다.");
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception exception) {
+        log.error(exception.getMessage());
+
+        return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(exception.getMessage());
+                .body("서버 내부적인 문제가 발생했습니다.");
     }
 }
