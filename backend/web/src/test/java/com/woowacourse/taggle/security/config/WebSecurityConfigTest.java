5d695ae2304a1d2a32eaf202050a08f083278cd7
KS-KIM <nullable@kakao.com>
2020-07-31 03:13:27 +0900

    refactor: 태그 및 북마크 API의 예외처리를 위한 ControllerAdvice 추가 (#91)
    * fix(backend): spring security 설정 일시적으로 해제
    
    * refactor(backend): 예외 케이스 처리 및 로깅 설정

--- backend/web/src/test/java/com/woowacourse/taggle/security/config/WebSecurityConfigTest.java
+++ backend/web/src/test/java/com/woowacourse/taggle/security/config/WebSecurityConfigTest.java
@@ -1,14 +1,8 @@
 package com.woowacourse.taggle.security.config;
 
-import static io.restassured.RestAssured.*;
-
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
 
 import io.restassured.RestAssured;
 
@@ -26,17 +20,18 @@ class WebSecurityConfigTest {
         RestAssured.port = port;
     }
 
+    // @TODO 인증 해제로 인한 임시 해제 조치
     // @formatter:off
-    @DisplayName("로그인이 되어있지 않는다면 로그인페이지로 리다이렉트")
-    @Test
-    void notLoginReturnRedirect() {
-        given()
-        .when()
-                .redirects().follow(false)
-                .get("/tag")
-        .then()
-                .log().all()
-                .statusCode(HttpStatus.FOUND.value())
-                .header("Location", Matchers.containsString("/login"));
-    }
+    // @DisplayName("로그인이 되어있지 않다면 로그인 페이지로 리다이렉트한다.")
+    // @Test
+    // void notLoginReturnRedirect() {
+    //     given()
+    //     .when()
+    //             .redirects().follow(false)
+    //             .get("/api/v1/tags")
+    //     .then()
+    //             .log().all()
+    //             .statusCode(HttpStatus.FOUND.value())
+    //             .header("Location", Matchers.containsString("/login"));
+    // }
 }
