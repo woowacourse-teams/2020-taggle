fee0f9e772c1721bf7dab6a20cad5b21e9ca16c7
kimjunseok <jnunseok@gmail.com>
2020-08-04 16:07:58 +0900

    feat(category-tag): 카테고리 서비스 생성 구현

--- backend/core/src/test/java/com/woowacourse/taggle/tag/service/CategoryServiceTest.java
+++ backend/core/src/test/java/com/woowacourse/taggle/tag/service/CategoryServiceTest.java
@@ -5,24 +5,38 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.woowacourse.taggle.JpaTestConfiguration;
+import com.woowacourse.taggle.tag.dto.CategoryRequest;
+import com.woowacourse.taggle.tag.dto.CategoryResponse;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = JpaTestConfiguration.class)
 @DataJpaTest
 class CategoryServiceTest {
 
-    @DisplayName("displayName")
+    @Autowired
+    private CategoryService categoryService;
+
+    @DisplayName("createCategory: 카테고리를 추가한다.")
     @Test
     void createCategory() {
+        // given
+        final CategoryRequest categoryRequest = new CategoryRequest("project");
 
+        // when
+        final CategoryResponse categoryResponse = categoryService.createCategory(categoryRequest);
+
+        // than
+        assertThat(categoryResponse.getId()).isNotNull();
+        assertThat(categoryResponse.getTitle()).isEqualTo("project");
     }
 
-    @DisplayName("displayName")
+    @DisplayName("findCategories: 카테고리 목록을 가져온다.")
     @Test
     void findCategories() {
     }
