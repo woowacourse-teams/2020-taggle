2b6fa31df16076d68a46f84aae49c37ea7b928bb
taejinseok <stj930@gmail.com>
2020-08-10 17:08:24 +0900

    refactor: alt 피드백 반영

--- backend/core/src/main/java/com/woowacourse/taggle/tag/domain/CategoryRepository.java
+++ backend/core/src/main/java/com/woowacourse/taggle/tag/domain/CategoryRepository.java
@@ -7,5 +7,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
     Optional<Category> findByTitle(final String title);
 }
