d738cc7157ad9157f48b42a1acfe8b8063c0d695
SeYun <ksy90101@naver.com>
2020-08-12 00:48:08 +0900

    feat(CategoryAddModal): 카테고리 추가 모달 레이아웃 생성

--- backend/web/src/frontend/src/views/header/BookmarkHeader.vue
+++ backend/web/src/frontend/src/views/header/BookmarkHeader.vue
@@ -19,23 +19,54 @@
               </v-list-item-content>
             </v-list-item>
           </v-list>
+          <template v-slot:append>
+            <v-avatar>
+              <img
+                src="https://avatars3.githubusercontent.com/u/45934117?s=460&u=32a2980278056093510eface6c7ddfca6bcd8bc9&v=4"
+                alt="evan"
+              />
+            </v-avatar>
+          </template>
         </v-navigation-drawer>
-        <v-card tile width="250">
-          <v-list v-for="{ id, title, tags } in categories" :key="id" class="grow">
-            <v-list-group class="text-left mt-2" link value="true">
-              <template v-slot:activator>
-                <v-list-item-content>
-                  <v-list-item-title class="font-weight-black text-h7">{{ title }}</v-list-item-title>
-                </v-list-item-content>
-              </template>
-              <v-list-item v-for="{ id, name } in tags" :key="id" @click.prevent="fetchBookmarks">
-                <v-list-item-content>
-                  {{ name }}
-                </v-list-item-content>
-              </v-list-item>
-            </v-list-group>
-          </v-list>
-        </v-card>
+        <v-col>
+          <v-navigation-drawer permanent width="100%">
+            <v-text-field
+              prepend-inner-icon="mdi-magnify"
+              placeholder="Tag Search"
+              filled
+              rounded
+              dense
+              class="ma-2"
+              style="height: 5%;"
+            ></v-text-field>
+            <v-card tile width="250" height="95%">
+              <v-list v-for="{ id, title, tags } in categories" :key="id" class="grow">
+                <v-list-group class="text-left mt-2" link value="true">
+                  <template v-slot:activator>
+                    <v-list-item-content>
+                      <v-list-item-title class="font-weight-black text-h7">{{ title }}</v-list-item-title>
+                    </v-list-item-content>
+                  </template>
+                  <v-list-item v-for="{ id, name } in tags" :key="id" @click.prevent="fetchBookmarks">
+                    <v-list-item-content>
+                      {{ name }}
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-list-group>
+              </v-list>
+            </v-card>
+            <template v-slot:append>
+              <v-row width="100%">
+                <v-col class="text-right">
+                  <CategoryAddModal> </CategoryAddModal>
+                  <v-btn class="mx-2" fab dark small color="indigo">
+                    <v-icon>mdi-brightness-7</v-icon>
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </template>
+          </v-navigation-drawer>
+        </v-col>
       </v-row>
     </v-navigation-drawer>
   </v-card>
@@ -43,9 +74,13 @@
 
 <script>
 import { categories } from '@/utils/mockTags.js';
+import CategoryAddModal from '@/views/header/component/CategoryAddModal.vue';
 
 export default {
   name: 'BookmarkHeader',
+  components: {
+    CategoryAddModal,
+  },
   methods: {
     fetchBookmarks() {
       if (this.$router.currentRoute.path !== '/bookmark') {
