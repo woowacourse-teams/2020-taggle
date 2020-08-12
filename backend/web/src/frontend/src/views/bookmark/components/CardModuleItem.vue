a79a6b112e39ff536d8562086fdd8de88776435c
taejinseok <stj930@gmail.com>
2020-08-12 16:41:53 +0900

    feat: 태그 편집 버튼, 모달창 추가

---
+++ backend/web/src/frontend/src/views/bookmark/components/CardModuleItem.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-card :href="bookmark.link" link class="ma-5" outlined target="_blank" width="300">
+    <v-responsive aspect-ratio="0.8">
+      <v-img :src="bookmark.img" aspect-ratio="1.6"></v-img>
+      <div class="test-size">
+        <v-card-title primary-title>
+          <span class="title-wrap">
+            {{ bookmark.title }}
+          </span>
+        </v-card-title>
+        <v-card-text>
+          <span class="content-wrap">
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem
+            aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni
+            dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor
+            sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore
+            magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis
+            suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in
+            ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas
+            nulla pariatur?
+          </span>
+        </v-card-text>
+      </div>
+    </v-responsive>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-share-variant</v-icon>
+      </v-btn>
+      <TagEditModal :bookmark="bookmark" />
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+import TagEditModal from './TagEditModal.vue';
+
+export default {
+  name: 'CardStreamItem',
+  components: { TagEditModal },
+  props: {
+    bookmark: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped></style>
--- backend/web/src/frontend/src/views/bookmark/components/CardStream.vue
+++ backend/web/src/frontend/src/views/bookmark/components/CardStream.vue
@@ -1,49 +1,17 @@
 <template>
   <v-row>
-    <v-col v-for="bookmark in bookmarks" :key="bookmark.id" cols="12">
-      <v-card :href="bookmark.link" class="d-flex" link outlined target="_blank">
-        <v-col align-self="center" class="flex-grow-0">
-          <v-avatar class="m-2" size="170" tile>
-            <v-img :src="bookmark.img"></v-img>
-          </v-avatar>
-        </v-col>
-        <v-list-item-content class="flex-grow-1">
-          <v-card-title primary-title>
-            <span class="title-wrap">
-              {{ bookmark.title }}
-            </span>
-          </v-card-title>
-          <v-card-text>
-            <span class="content-wrap">
-              Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam
-              rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt
-              explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
-              consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
-              dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora
-              incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
-              exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem
-              vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui
-              dolorem eum fugiat quo voluptas nulla pariatur?
-            </span>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn icon>
-              <v-icon>mdi-heart</v-icon>
-            </v-btn>
-            <v-btn icon>
-              <v-icon>mdi-share-variant</v-icon>
-            </v-btn>
-          </v-card-actions>
-        </v-list-item-content>
-      </v-card>
-    </v-col>
+    <CardStreamItem v-for="bookmark in bookmarks" :key="bookmark.id" :bookmark="bookmark" />
   </v-row>
 </template>
 
 <script>
+import CardStreamItem from './CardStreamItem.vue';
+
 export default {
   name: 'CardModule',
+  components: {
+    CardStreamItem,
+  },
   props: {
     bookmarks: Array,
   },
