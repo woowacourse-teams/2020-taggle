44d878b9d4306b24286b679d416c46d9daffaa2d
SeYun <53366407+ksy90101@users.noreply.github.com>
2020-08-13 20:37:59 +0900

    feat(frontend): 태그 목록에 태그 검색, 추가, 수정 레이아웃 추가 (#125)
    * feat(category): 카테고리 추가 모달 레이아웃 생성
    
    * chroe(frontend): vuedraggable 라이브러리 추가
    
    * feat(category): 카테고리의 태그 수정 모달 생성
    
    * refactor(tag): 피드백 적용

--- backend/web/src/frontend/src/views/header/BookmarkHeader.vue
+++ backend/web/src/frontend/src/views/header/BookmarkHeader.vue
@@ -19,35 +19,75 @@
               </v-list-item-content>
             </v-list-item>
           </v-list>
+          <template v-slot:append>
+            <div class="text-center">
+              <v-avatar>
+                <img
+                  src="https://avatars3.githubusercontent.com/u/45934117?s=460&u=32a2980278056093510eface6c7ddfca6bcd8bc9&v=4"
+                  alt="evan"
+                />
+              </v-avatar>
+            </div>
+          </template>
         </v-navigation-drawer>
-        <v-card tile width="250">
-          <v-list v-for="{ id, title, tags } in categories" :key="id" class="grow">
-            <v-list-group class="text-left mt-2" link value="true">
-              <template v-slot:activator>
-                <v-list-item-content>
-                  <v-list-item-title class="font-weight-black text-h7">{{ title }}</v-list-item-title>
-                </v-list-item-content>
-              </template>
-              <v-list-item v-for="{ id, name } in tags" :key="id" :to="{ name: 'bookmarks', params: { id } }">
-                <v-list-item-content>
-                  {{ name }}
-                </v-list-item-content>
-              </v-list-item>
-            </v-list-group>
-          </v-list>
-        </v-card>
+
+        <v-col>
+          <v-navigation-drawer permanent width="100%">
+            <v-text-field
+              prepend-inner-icon="mdi-magnify"
+              placeholder="Tag Search"
+              filled
+              rounded
+              dense
+              class="ma-2"
+              height="5%"
+            ></v-text-field>
+            <v-card tile width="250" height="95%">
+              <v-list v-for="{ id, title, tags } in categories" :key="id" class="grow">
+                <v-list-group class="text-left mt-2" link value="true">
+                  <template v-slot:activator>
+                    <v-list-item-content>
+                      <v-list-item-title class="font-weight-black text-h7">
+                        {{ title }}
+                      </v-list-item-title>
+                    </v-list-item-content>
+                  </template>
+                  <v-list-item v-for="{ id, name } in tags" :key="id" :to="{ name: 'bookmarks', params: { id } }">
+                    <v-list-item-content>
+                      {{ name }}
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-list-group>
+              </v-list>
+            </v-card>
+            <template v-slot:append>
+              <v-row width="100%">
+                <v-col class="text-right">
+                  <CategoryAddModal></CategoryAddModal>
+                  <CategoryTagModifyModal></CategoryTagModifyModal>
+                </v-col>
+              </v-row>
+            </template>
+          </v-navigation-drawer>
+        </v-col>
       </v-row>
     </v-navigation-drawer>
   </v-card>
 </template>
 
 <script>
+import CategoryAddModal from '@/views/header/component/CategoryAddModal.vue';
+import CategoryTagModifyModal from '@/views/header/component/TagEditModal.vue';
 import { FETCH_CATEGORIES } from '@/store/share/actionTypes.js';
 import { CATEGORIES } from '@/store/share/getterTypes.js';
 import { mapActions, mapGetters } from 'vuex';
 
 export default {
   name: 'BookmarkHeader',
+  components: {
+    CategoryAddModal,
+    CategoryTagModifyModal,
+  },
   created() {
     this[FETCH_CATEGORIES]();
   },
