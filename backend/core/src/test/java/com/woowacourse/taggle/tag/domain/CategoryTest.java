f125bee82bd21fcbc49ff3e906d0fb4fdff8c9be
Ramen6315 <ramen6315@gmail.com>
2020-08-13 10:44:36 +0900

    test(Category): core 테스트 코드 수정

--- backend/core/src/test/java/com/woowacourse/taggle/tag/domain/CategoryTest.java
+++ backend/core/src/test/java/com/woowacourse/taggle/tag/domain/CategoryTest.java
@@ -5,12 +5,23 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import com.woowacourse.taggle.user.domain.Role;
+import com.woowacourse.taggle.user.domain.User;
+
 public class CategoryTest {
 
     @DisplayName("constructor: 카테고리를 생성한다.")
     @Test
     void constructor() {
-        assertThat(new Category("project")).isInstanceOf(Category.class);
+        User user = User.builder()
+                .id(1L)
+                .email("a@a.com")
+                .nickName("tigger")
+                .role(Role.USER)
+                .picture("https://www.naver.com/")
+                .build();
+
+        assertThat(new Category("project", user)).isInstanceOf(Category.class);
     }
 
 }
