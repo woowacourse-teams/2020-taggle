e3a2abcd76df8202a7dac496c3d4915a8ad72f87
taejinseok <stj930@gmail.com>
2020-08-15 21:13:32 +0900

    feat(frontend): 북마크 페이지, 태그 편집 모달창 태그 삭제 기능
    - 기존에는 특정 태그의 북마크 목록 페이지 랜더링시, 각 북마크의 모든 태그 목록들이 바로 랜더링
    - 특정 북마크 태그 편집 모달창 활성화 시, 북마크의 태그목록 요청하는 방법으로 수정

--- backend/web/src/frontend/src/views/bookmark/components/TagEditModal.vue
+++ backend/web/src/frontend/src/views/bookmark/components/TagEditModal.vue
@@ -16,6 +16,7 @@
             :tags="tags"
             @tags-changed="(newTags) => (tags = newTags)"
             @before-adding-tag="onAddTagBookmark"
+            @before-deleting-tag="onDeleteTagBookmark"
             placeholder="해당 북마크에 태그를 추가하거나 삭제할 수 있습니다."
           />
         </v-card-text>
@@ -29,7 +30,7 @@ import VueTagsInput from '@johmun/vue-tags-input';
 import { mapActions, mapGetters, mapMutations } from 'vuex';
 import { SHOW_SNACKBAR } from '@/store/share/mutationTypes.js';
 import TagService from '@/api/module/tag.js';
-import { BOOKMARK_TAGS } from '@/store/share/getterTypes.js';
+import { BOOKMARK_TAGS, GET_TAG_ID_BY_NAME } from '@/store/share/getterTypes.js';
 import { FETCH_BOOKMARK_TAGS, FETCH_CATEGORIES } from '@/store/share/actionTypes.js';
 
 export default {
@@ -86,6 +87,16 @@ export default {
         await this[FETCH_CATEGORIES]();
       } catch (e) {
         this[SHOW_SNACKBAR]('태그 북마크 생성중 오류가 발생했습니다.');
+      }
+    },
+    async onDeleteTagBookmark(data) {
+      const deleteName = data.tag.text;
+      const tagId = this[GET_TAG_ID_BY_NAME](deleteName);
+      try {
+        await TagService.deleteBookmarkOnTag(tagId, this.bookmark.id);
+        data.deleteTag();
+      } catch (e) {
+        this[SHOW_SNACKBAR]('태그 북마크 삭제중 오류가 발생했습니다.');
       }
     },
     initTags() {
