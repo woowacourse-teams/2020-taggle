4c0ffcde170b93d498d6336f84ffafd4c60029b5
SeYun <ksy90101@naver.com>
2020-08-17 20:06:16 +0900

    refactor(tagList): 태그 리스트 컴포넌트 분리

--- backend/web/src/frontend/src/views/header/BookmarkHeader.vue
+++ backend/web/src/frontend/src/views/header/BookmarkHeader.vue
@@ -37,73 +37,21 @@
         </template>
       </v-navigation-drawer>
 
-      <v-navigation-drawer permanent>
-        <!-- tag search bar -->
-        <v-text-field
-          prepend-inner-icon="mdi-magnify"
-          placeholder="Tag Search"
-          filled
-          rounded
-          dense
-          class="ma-2"
-          hide-details="true"
-        ></v-text-field>
-
-        <!-- tags & categories -->
-        <v-list v-for="{ id, title, tags } in categories" :key="id" class="grow">
-          <v-list-group class="text-left" link value="true">
-            <template v-slot:activator>
-              <v-list-item-content>
-                <v-list-item-title class="font-weight-black text-h7">
-                  {{ title }}
-                </v-list-item-title>
-              </v-list-item-content>
-            </template>
-            <v-list-item v-for="{ id, name } in tags" :key="id" :to="{ name: 'bookmarks', params: { id } }">
-              <v-list-item-content>
-                {{ name }}
-              </v-list-item-content>
-            </v-list-item>
-          </v-list-group>
-        </v-list>
-        <!-- category modify buttons -->
-        <template v-slot:append>
-          <v-divider></v-divider>
-          <v-row width="100%">
-            <v-col class="text-right">
-              <CategoryAddModal />
-              <CategoryTagModifyModal />
-            </v-col>
-          </v-row>
-        </template>
-      </v-navigation-drawer>
+      <!-- tags & categories -->
+      <tagList />
     </v-row>
   </v-navigation-drawer>
 </template>
 
 <script>
-import CategoryAddModal from '@/views/header/component/CategoryAddModal.vue';
-import CategoryTagModifyModal from '@/views/header/component/TagEditModal.vue';
 import BookmarkAddModal from '@/views/header/component/BookmarkAddModal.vue';
-import { FETCH_CATEGORIES } from '@/store/share/actionTypes.js';
-import { CATEGORIES } from '@/store/share/getterTypes.js';
-import { mapActions, mapGetters } from 'vuex';
+import TagList from '@/views/header/component/TagList.vue';
 
 export default {
   name: 'BookmarkHeader',
   components: {
-    CategoryAddModal,
-    CategoryTagModifyModal,
     BookmarkAddModal,
-  },
-  created() {
-    this[FETCH_CATEGORIES]();
-  },
-  methods: {
-    ...mapActions([FETCH_CATEGORIES]),
-  },
-  computed: {
-    ...mapGetters([CATEGORIES]),
+    TagList,
   },
 };
 </script>
