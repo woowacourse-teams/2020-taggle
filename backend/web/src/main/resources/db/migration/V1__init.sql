18f3a9c42b3be1e594a6e0723115e0706ea39c4a
Ramen6315 <ramen6315@gmail.com>
2020-08-18 18:07:40 +0900

    refactor(backend): sql, application-db 변경

--- backend/web/src/main/resources/db/migration/V1__init.sql
+++ backend/web/src/main/resources/db/migration/V1__init.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `user`
 (
-    `id`            bigint(20)   NOT NULL AUTO_INCREMENT,
+    `user_id`            bigint(20)   NOT NULL AUTO_INCREMENT,
     `created_date`  datetime     DEFAULT NULL,
     `modified_date` datetime     DEFAULT NULL,
     `email`         varchar(255) NOT NULL,
@@ -9,7 +9,7 @@ CREATE TABLE `user`
     `picture`       longtext NOT NULL,
     `role`          varchar(255) NOT NULL,
     `sign_out_date` datetime     DEFAULT NULL,
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`user_id`)
 );
 
 CREATE TABLE `bookmark`
@@ -22,7 +22,7 @@ CREATE TABLE `bookmark`
     `title` varchar(255) DEFAULT NULL,
     `image` longtext DEFAULT NULL,
     PRIMARY KEY (`bookmark_id`),
-    FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+    FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
 );
 
 CREATE TABLE `category`
@@ -31,26 +31,26 @@ CREATE TABLE `category`
     `title`       varchar(255) NOT NULL,
     `user_id`     bigint(20) DEFAULT NULL,
     PRIMARY KEY (`category_id`),
-    FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+    FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
 );
 
 CREATE TABLE `tag`
 (
     `tag_id`               bigint(20)   NOT NULL AUTO_INCREMENT,
     `name`                 varchar(255) NOT NULL,
-    `category_category_id` bigint(20) DEFAULT NULL,
+    `category_id` bigint(20) DEFAULT NULL,
     `user_id`              bigint(20) DEFAULT NULL,
     PRIMARY KEY (`tag_id`),
-    FOREIGN KEY (`category_category_id`) REFERENCES `category` (`category_id`),
-    FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+    FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`),
+    FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
 );
 
 CREATE TABLE `tag_bookmark`
 (
-    `id`                   bigint(20) NOT NULL AUTO_INCREMENT,
-    `bookmark_bookmark_id` bigint(20) DEFAULT NULL,
-    `tag_tag_id`           bigint(20) DEFAULT NULL,
-    PRIMARY KEY (`id`),
-    FOREIGN KEY (`bookmark_bookmark_id`) REFERENCES `bookmark` (`bookmark_id`),
-    FOREIGN KEY (`tag_tag_id`) REFERENCES `tag` (`tag_id`)
+    `tag_bookmark_id`                   bigint(20) NOT NULL AUTO_INCREMENT,
+    `bookmark_id` bigint(20) DEFAULT NULL,
+    `tag_id`           bigint(20) DEFAULT NULL,
+    PRIMARY KEY (`tag_bookmark_id`),
+    FOREIGN KEY (`bookmark_id`) REFERENCES `bookmark` (`bookmark_id`),
+    FOREIGN KEY (`tag_id`) REFERENCES `tag` (`tag_id`)
 );
