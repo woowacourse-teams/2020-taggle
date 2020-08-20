5759a62fba468cd5900d200b4143f9c512819942
Ramen6315 <ramen6315@gmail.com>
2020-08-20 20:50:31 +0900

    refactor(backend): sql 문 수정

--- backend/web/src/main/resources/db/migration/V1__init.sql
+++ backend/web/src/main/resources/db/migration/V1__init.sql
@@ -1,12 +1,12 @@
 CREATE TABLE `user`
 (
-    `user_id`            bigint(20)   NOT NULL AUTO_INCREMENT,
+    `user_id`       bigint(20)   NOT NULL AUTO_INCREMENT,
     `created_date`  datetime     DEFAULT NULL,
     `modified_date` datetime     DEFAULT NULL,
     `email`         varchar(255) NOT NULL,
     `nick_name`     varchar(255) NOT NULL,
     `phone_number`  varchar(255) DEFAULT NULL,
-    `picture`       text NOT NULL,
+    `picture`       text         NOT NULL,
     `role`          varchar(255) NOT NULL,
     `sign_out_date` datetime     DEFAULT NULL,
     PRIMARY KEY (`user_id`)
@@ -14,13 +14,13 @@ CREATE TABLE `user`
 
 CREATE TABLE `bookmark`
 (
-    `bookmark_id` bigint(20)   NOT NULL AUTO_INCREMENT,
-    `is_read`     bit(1)       NOT NULL,
-    `url`         text NOT NULL,
-    `user_id`     bigint(20) DEFAULT NULL,
-    `description` text DEFAULT NULL,
-    `title` varchar(255) DEFAULT NULL,
-    `image` text DEFAULT NULL,
+    `bookmark_id` bigint(20) NOT NULL AUTO_INCREMENT,
+    `is_read`     bit(1)     NOT NULL,
+    `url`         text       NOT NULL,
+    `user_id`     bigint(20)   DEFAULT NULL,
+    `description` text         DEFAULT NULL,
+    `title`       varchar(255) DEFAULT NULL,
+    `image`       text         DEFAULT NULL,
     PRIMARY KEY (`bookmark_id`),
     FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
 );
@@ -36,10 +36,10 @@ CREATE TABLE `category`
 
 CREATE TABLE `tag`
 (
-    `tag_id`               bigint(20)   NOT NULL AUTO_INCREMENT,
-    `name`                 varchar(255) NOT NULL,
+    `tag_id`      bigint(20)   NOT NULL AUTO_INCREMENT,
+    `name`        varchar(255) NOT NULL,
     `category_id` bigint(20) DEFAULT NULL,
-    `user_id`              bigint(20) DEFAULT NULL,
+    `user_id`     bigint(20) DEFAULT NULL,
     PRIMARY KEY (`tag_id`),
     FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`),
     FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
@@ -47,9 +47,9 @@ CREATE TABLE `tag`
 
 CREATE TABLE `tag_bookmark`
 (
-    `tag_bookmark_id`                   bigint(20) NOT NULL AUTO_INCREMENT,
-    `bookmark_id` bigint(20) DEFAULT NULL,
-    `tag_id`           bigint(20) DEFAULT NULL,
+    `tag_bookmark_id` bigint(20) NOT NULL AUTO_INCREMENT,
+    `bookmark_id`     bigint(20) DEFAULT NULL,
+    `tag_id`          bigint(20) DEFAULT NULL,
     PRIMARY KEY (`tag_bookmark_id`),
     FOREIGN KEY (`bookmark_id`) REFERENCES `bookmark` (`bookmark_id`),
     FOREIGN KEY (`tag_id`) REFERENCES `tag` (`tag_id`)
