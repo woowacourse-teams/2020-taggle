49be99bd66cb3003b33160def2ac141aab15a0bd
Taejin Seok <46241671+taejinseok@users.noreply.github.com>
2020-08-21 15:40:55 +0900

    feat(chrome-extension): API 연동 (#110)
    * chore: eslint import문 확장자 명시 가능하도록 설정 변경
    
    * feat: mock데이터 추가
    
    * chore: vuex 의존성 추가
    
    * refactor: mock 데이터 테스트용으로 api 연결
    
    * refactor: api 호출 mapActions 활용
    
    * feat: 실제 api 연동
    
    * refactor: 컴포넌트내부 옵션 순서 스타일가이드 기준 정렬
    
    * chore: vue/eslint-config-prettier 의존성 추가
    
    * style: prettier 적용
    
    * refactor: 1차 피드백 반영
    
    * feat: 2차 피드백 반영 리팩토링
    
    * refactor: 3차 피드백 반영 리팩토링
    
    * refactor: 메서드명, 일부 로직 이동 등 리팩토링
    
    - method명 리팩토링
    - input 컴포넌트 내 북마크 저장 로직 buttons 컴포넌트로 이동
    - extension 프로그램 이름, 설명 수정
    - 팝업창 아이콘 클릭시 taggle.kr로 이동
    - snackbar getter 상수 분리
    
    * refactor(auth): API 호출에 credential 적용
    
    - 로그인이 되어있지 않은 경우 태그를 추가할 수 없도록 팝업을 닫음
    - 로그인이 되어있지 않은 경우 서비스 로그인 페이지로 리다이렉트
    - 로그인이 되어있는 경우 taggle 서비스에서 쿠키를 가져올 수 있음
    
    Co-authored-by: K.S.KIM <nullable@kakao.com>

--- taggle-extension/src/api/module/tag.js
+++ taggle-extension/src/api/module/tag.js
@@ -1,13 +1,16 @@
-import ApiService from '../index.js';
+import ApiService from '@/api/index.js';
 
 const BASE_URL = '/api/v1/tags';
 
 const TagService = {
   create(newTag) {
-    return ApiService.post(`${BASE_URL}`, newTag).then((location) => {
-      const locations = location.split('/');
-      return locations[locations.length - 1];
-    });
+    return ApiService.post(BASE_URL, newTag).then(({ data }) => data.id);
+  },
+  addBookmarkOnTag(bookmarkId, tagId) {
+    return ApiService.post(`${BASE_URL}/${tagId}/bookmarks/${bookmarkId}`);
+  },
+  deleteBookmarkOnTag(bookmarkId, tagId) {
+    return ApiService.delete(`${BASE_URL}/${tagId}/bookmarks/${bookmarkId}`);
   },
 };
 
---
+++ taggle-extension/src/store/index.js
@@ -0,0 +1,81 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import {
+  ADD_TAG_ON_BOOKMARK,
+  CREATE_TAG,
+  DELETE_BOOKMARK,
+  DELETE_TAG_ON_BOOKMARK,
+  CREATE_BOOKMARK,
+  FETCH_BOOKMARK_WITH_TAGS,
+} from '@/store/share/actionsType.js';
+import TagService from '@/api/module/tag.js';
+import BookmarkService from '@/api/module/bookmark.js';
+import { RESET_BOOKMARK, SET_BOOKMARK, SHOW_SNACKBAR } from '@/store/share/mutationsType.js';
+import { BOOKMARK_ID, IS_SHOW, MESSAGE, GET_TAG_ID_BY_NAME } from '@/store/share/gettersType.js';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    bookmarkWithTags: {
+      id: 0,
+      tags: [
+        {
+          id: 0,
+          name: '',
+        },
+      ],
+    },
+    isShow: false,
+    message: '',
+  },
+  getters: {
+    [BOOKMARK_ID](state) {
+      return state.bookmarkWithTags.id;
+    },
+    [GET_TAG_ID_BY_NAME]: (state) => (name) => {
+      const tag = state.bookmarkWithTags.tags.find((item) => item.name === name);
+      return tag ? tag.id : undefined;
+    },
+    [IS_SHOW](state) {
+      return state.isShow;
+    },
+    [MESSAGE](state) {
+      return state.message;
+    },
+  },
+  actions: {
+    async [FETCH_BOOKMARK_WITH_TAGS]({ commit }, bookmarkId) {
+      const bookmarkResponse = await BookmarkService.getBookmarkWithTags(bookmarkId);
+      commit(SET_BOOKMARK, bookmarkResponse);
+    },
+    async [CREATE_TAG](context, newTag) {
+      return TagService.create(newTag);
+    },
+    async [ADD_TAG_ON_BOOKMARK](context, { bookmarkId, tagId }) {
+      return TagService.addBookmarkOnTag(bookmarkId, tagId);
+    },
+    async [DELETE_TAG_ON_BOOKMARK](context, { bookmarkId, tagId }) {
+      return TagService.deleteBookmarkOnTag(bookmarkId, tagId);
+    },
+    async [CREATE_BOOKMARK](context, bookmark) {
+      return BookmarkService.create(bookmark);
+    },
+    async [DELETE_BOOKMARK](context) {
+      await BookmarkService.delete(context.state.bookmarkWithTags.id);
+      context.commit(RESET_BOOKMARK);
+    },
+  },
+  mutations: {
+    [SET_BOOKMARK](state, bookmark) {
+      state.bookmarkWithTags = bookmark;
+    },
+    [RESET_BOOKMARK](state) {
+      state.bookmarkWithTags = {};
+    },
+    [SHOW_SNACKBAR](state, message) {
+      state.isShow = !state.isShow;
+      state.message = message;
+    },
+  },
+});
