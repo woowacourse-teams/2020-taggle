07f7e66e813061b5fa5558103b937a044eb6ee47
Ramen6315 <ramen6315@gmail.com>
2020-08-21 20:45:42 +0900

    feat(frontend): google analytics 설정

--- backend/web/src/frontend/src/main.js
+++ backend/web/src/frontend/src/main.js
@@ -3,12 +3,17 @@ import App from './App.vue';
 import router from './router/index.js';
 import store from './store/index.js';
 import vuetify from './plugins/vuetify.js';
+import vueGtag from "vue-gtag";
 
 Vue.config.productionTip = false;
 
+Vue.use(vueGtag, {
+    config: {id: "UA-175962196-1"}
+}, router);
+
 new Vue({
-  router,
-  store,
-  vuetify,
-  render: (h) => h(App),
+    router,
+    store,
+    vuetify,
+    render: (h) => h(App),
 }).$mount('#app');
