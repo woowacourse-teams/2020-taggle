cecfb0b62587fe4ab850b56d06ab33e3f8310e23
Ramen6315 <ramen6315@gmail.com>
2020-08-22 14:53:19 +0900

    refactor(infra): 피드백 반영

--- backend/web/src/frontend/src/main.js
+++ backend/web/src/frontend/src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import vueGtag from "vue-gtag";
+import VueGtag from 'vue-gtag';
 import App from './App.vue';
 import router from './router/index.js';
 import store from './store/index.js';
@@ -7,13 +7,17 @@ import vuetify from './plugins/vuetify.js';
 
 Vue.config.productionTip = false;
 
-Vue.use(vueGtag, {
-  config: {id: "UA-175962196-1"}
-}, router);
+Vue.use(
+  VueGtag,
+  {
+    config: { id: 'UA-175962196-1' },
+  },
+  router,
+);
 
 new Vue({
   router,
-  store, 
+  store,
   vuetify,
   render: (h) => h(App),
 }).$mount('#app');
