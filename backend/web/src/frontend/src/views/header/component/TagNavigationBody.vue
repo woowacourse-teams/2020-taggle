141dc94cf759e03be342877253f94633116f8f87
taejinseok <stj930@gmail.com>
2020-08-25 20:24:11 +0900

    refactor(frontend): 컴포넌트내 불필요 style 태그 제거

--- backend/web/src/frontend/src/views/header/component/TagNavigationBody.vue
+++ backend/web/src/frontend/src/views/header/component/TagNavigationBody.vue
@@ -1,43 +1,21 @@
 <template>
   <v-card flat="true">
-    <v-list v-for="{ id, title, tags } in categories" :key="id" class="grow">
-      <v-list-group sub-group class="text-left" value="true">
-        <template v-slot:activator>
-          <v-list-item-title class="font-weight-black text-h7">
-            {{ title }}
-          </v-list-item-title>
-          <v-list-item-action v-if="id">
-            <CategoryContextMenu :category="{ id, title }" />
-          </v-list-item-action>
-        </template>
-        <v-list-item v-for="{ id, name } in tags" :key="id" :to="{ name: 'bookmarks', params: { id } }">
-          <!--            <v-list-item-title @click.prevent="">-->
-          <!--              <v-text-field :value="name" />-->
-          <!--            </v-list-item-title>-->
-          <v-list-item-title>
-            {{ name }}
-          </v-list-item-title>
-          <v-list-item-action>
-            <TagContextMenu :tag="{ id, title }" />
-          </v-list-item-action>
-        </v-list-item>
-      </v-list-group>
+    <v-list v-for="category in categories" :key="category.id" class="grow">
+      <CategoryGroup :category="category" />
     </v-list>
   </v-card>
 </template>
 
 <script>
-import CategoryContextMenu from '@/views/header/component/category/CategoryContextMenu.vue';
-import TagContextMenu from '@/views/header/component/category/TagContextMenu.vue';
 import { FETCH_CATEGORIES } from '@/store/share/actionTypes.js';
 import { mapActions, mapGetters } from 'vuex';
 import { CATEGORIES } from '@/store/share/getterTypes.js';
+import CategoryGroup from '@/views/header/component/CategoryGroup.vue';
 
 export default {
   name: 'TagNavigationBody',
   components: {
-    CategoryContextMenu,
-    TagContextMenu,
+    CategoryGroup,
   },
   computed: {
     ...mapGetters([CATEGORIES]),
@@ -50,5 +28,3 @@ export default {
   },
 };
 </script>
-
-<style scoped></style>
