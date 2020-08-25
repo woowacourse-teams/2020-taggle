260b366df3decfc94a9e2acc2778b80d457766cf
taejinseok <stj930@gmail.com>
2020-08-25 19:20:40 +0900

    refactor(frontend): header navigation bar divider 제거

--- backend/web/src/frontend/src/views/header/BookmarkHeader.vue
+++ backend/web/src/frontend/src/views/header/BookmarkHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-navigation-drawer app permanent width="312px" floating>
+  <v-navigation-drawer app permanent width="312px">
     <v-row class="fill-height" no-gutters>
       <v-navigation-drawer dark mini-variant mini-variant-width="56" permanent>
         <!-- home icon -->
@@ -8,21 +8,6 @@
             <v-img src="@/assets/images/TaggleLog.png"></v-img>
           </v-list-item-avatar>
         </v-list-item>
-        <v-divider></v-divider>
-
-        <!-- navigation menus -->
-        <v-list dense nav>
-          <!--          dialog 컴포넌트화로 인해서 기존 버튼과의 불일치 발생. 해결 필요-->
-          <!--          <v-list-item link>-->
-          <!--            <v-list-item-action>-->
-          <!--              <v-icon>local_offer</v-icon>-->
-          <!--            </v-list-item-action>-->
-          <!--            <v-list-item-content>-->
-          <!--              <v-list-item-title>tags</v-list-item-title>-->
-          <!--            </v-list-item-content>-->
-          <!--          </v-list-item>-->
-          <BookmarkAddModal />
-        </v-list>
 
         <!--user information button-->
         <template v-slot:append>
@@ -38,20 +23,18 @@
       </v-navigation-drawer>
 
       <!-- tags & categories -->
-      <tagList />
+      <TagNavigation />
     </v-row>
   </v-navigation-drawer>
 </template>
 
 <script>
-import BookmarkAddModal from '@/views/header/component/BookmarkAddModal.vue';
-import TagList from '@/views/header/component/TagList.vue';
+import TagNavigation from '@/views/header/component/TagNavigation.vue';
 
 export default {
   name: 'BookmarkHeader',
   components: {
-    BookmarkAddModal,
-    TagList,
+    TagNavigation,
   },
 };
 </script>
