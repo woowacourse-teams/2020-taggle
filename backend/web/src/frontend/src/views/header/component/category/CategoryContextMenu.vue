7a087e84327044ddf52c310ee2af74195c0ae395
taejinseok <stj930@gmail.com>
2020-08-25 14:55:33 +0900

    feat(frontend): 카테고리, 태그 선택 버튼 append-icon으로 context menu 버튼 추가

---
+++ backend/web/src/frontend/src/views/header/component/category/CategoryContextMenu.vue
@@ -0,0 +1,27 @@
+<template>
+  <ContextMenu>
+    <template slot="menuItems">
+      <v-list-item v-for="(item, index) in items" :key="index" @click.prevent="">
+        <v-list-item-title>{{ item.title }}</v-list-item-title>
+      </v-list-item>
+    </template>
+  </ContextMenu>
+</template>
+
+<script>
+import ContextMenu from '@/views/header/component/category/ContextMenu.vue';
+
+export default {
+  name: 'CategoryContextMenu',
+  components: {
+    ContextMenu,
+  },
+  data() {
+    return {
+      items: [{ title: '카테고리수정' }, { title: '카테고리삭제' }],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ backend/web/src/frontend/src/views/header/component/category/TagContextMenu.vue
@@ -0,0 +1,27 @@
+<template>
+  <ContextMenu>
+    <template slot="menuItems">
+      <v-list-item v-for="(item, index) in items" :key="index" @click.prevent="">
+        <v-list-item-title>{{ item.title }}</v-list-item-title>
+      </v-list-item>
+    </template>
+  </ContextMenu>
+</template>
+
+<script>
+import ContextMenu from '@/views/header/component/category/ContextMenu.vue';
+
+export default {
+  name: 'TagContextMenu',
+  components: {
+    ContextMenu,
+  },
+  data() {
+    return {
+      items: [{ title: '태그수정' }, { title: '태그삭제' }],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
