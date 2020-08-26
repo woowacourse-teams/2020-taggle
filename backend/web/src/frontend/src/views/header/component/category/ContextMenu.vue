3f1b55927da1e3784d77e2c7cd8a3eaedc93d7fa
taejinseok <stj930@gmail.com>
2020-08-26 22:20:45 +0900

    feat(frontend): tag 카테고리 변경 메뉴 dialog 띄우기

--- backend/web/src/frontend/src/views/header/component/category/ContextMenu.vue
+++ backend/web/src/frontend/src/views/header/component/category/ContextMenu.vue
@@ -1,15 +1,18 @@
 <template>
-  <v-menu bottom :offset-x="true">
-    <template v-slot:activator="{ on, attrs }">
-      <v-btn icon v-bind="attrs" v-on="on" @click.prevent="">
-        <v-icon>mdi-dots-horizontal</v-icon>
-      </v-btn>
-    </template>
+  <v-layout>
+    <v-menu bottom :offset-x="true">
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn icon v-bind="attrs" v-on="on" @click.prevent="">
+          <v-icon>mdi-dots-horizontal</v-icon>
+        </v-btn>
+      </template>
 
-    <v-list>
-      <slot name="menuItems" />
-    </v-list>
-  </v-menu>
+      <v-list>
+        <slot name="menuItems" />
+      </v-list>
+    </v-menu>
+    <slot name="menuDialog" />
+  </v-layout>
 </template>
 
 <script>
