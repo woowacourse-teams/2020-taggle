5f1062456c137312f8c7552cd10fe75274ecf8f7
taejinseok <stj930@gmail.com>
2020-08-26 22:20:45 +0900

    feat(frontend): tag 카테고리 변경 메뉴 dialog 띄우기

--- backend/web/src/frontend/src/views/header/component/category/CategoryEditModal.vue
+++ backend/web/src/frontend/src/views/header/component/category/CategoryEditModal.vue
@@ -1,16 +1,8 @@
 <template>
   <v-dialog v-model="dialog" width="500">
-    <template v-slot:activator="{ on, attrs }">
-      <v-btn v-bind="attrs" v-on="on" class="mx-2" small icon>
-        <v-icon dark>mdi-pencil</v-icon>
-      </v-btn>
-    </template>
-
     <v-card>
       <v-app-bar dense flat>
-        <v-card-title>
-          카테고리 수정
-        </v-card-title>
+        <v-card-title>카테고리 변경</v-card-title>
         <v-spacer></v-spacer>
         <v-btn icon @click="dialog = false">
           <v-icon>mdi-close</v-icon>
@@ -18,54 +10,47 @@
       </v-app-bar>
 
       <v-card-text class="pt-6">
-        <v-form ref="category" @submit.prevent="onEditCategory">
-          <v-text-field
-            v-model="category.title"
-            dense
-            :rules="rules"
-            label="이름을 입력후에 enter를 입력하여 저장."
-          ></v-text-field>
-        </v-form>
+        변경내용
+        <!--        <v-form ref="category" @submit.prevent="onEditCategory">-->
+        <!--          <v-text-field-->
+        <!--            v-model="category.title"-->
+        <!--            dense-->
+        <!--            :rules="rules"-->
+        <!--            label="이름을 입력후에 enter를 입력하여 저장."-->
+        <!--          ></v-text-field>-->
+        <!--        </v-form>-->
       </v-card-text>
     </v-card>
   </v-dialog>
 </template>
 
 <script>
-import { mapActions, mapMutations } from 'vuex';
-import { EDIT_CATEGORY, FETCH_CATEGORIES } from '@/store/share/actionTypes.js';
-import { SHOW_SNACKBAR } from '@/store/share/mutationTypes.js';
-import { MESSAGES } from '@/utils/constants.js';
-import validator from '@/utils/validator.js';
-
 export default {
-  name: 'CategoryAddModal',
+  name: 'CategoryEditModal',
   data() {
     return {
-      rules: validator.category.title,
       dialog: false,
     };
   },
   props: {
-    category: {
+    tag: {
       type: Object,
       required: true,
     },
+    editDialog: {
+      type: Boolean,
+      required: true,
+    },
   },
-  methods: {
-    ...mapActions([EDIT_CATEGORY, FETCH_CATEGORIES]),
-    ...mapMutations([SHOW_SNACKBAR]),
-    async onEditCategory() {
-      if (!this.$refs.category.validate()) {
-        return;
+  watch: {
+    editDialog(value) {
+      if (value) {
+        this.dialog = value;
       }
-      try {
-        await this[EDIT_CATEGORY](this.category);
-        await this[FETCH_CATEGORIES]();
-        this[SHOW_SNACKBAR](MESSAGES.CATEGORY.EDIT.SUCCESS);
-        this.dialog = false;
-      } catch (e) {
-        this[SHOW_SNACKBAR](MESSAGES.CATEGORY.EDIT.FAIL);
+    },
+    dialog() {
+      if (!this.dialog) {
+        this.$emit('close');
       }
     },
   },
--- backend/web/src/frontend/src/views/header/component/category/TagContextMenu.vue
+++ backend/web/src/frontend/src/views/header/component/category/TagContextMenu.vue
@@ -1,7 +1,7 @@
 <template>
   <ContextMenu>
     <template slot="menuItems">
-      <v-list-item v-for="(item, index) in items" :key="index" @click.prevent="item.action">
+      <v-list-item v-for="(item, index) in items" :key="index" @click="item.action">
         <v-list-item-title>{{ item.title }}</v-list-item-title>
         <v-list-item-action>
           <v-icon>
@@ -10,21 +10,26 @@
         </v-list-item-action>
       </v-list-item>
     </template>
+    <template slot="menuDialog">
+      <CategoryEditModal :tag="tag" :editDialog="editCategoryDialog" @close="editCategoryDialog = false" />
+    </template>
   </ContextMenu>
 </template>
 
 <script>
-import ContextMenu from '@/views/header/component/category/ContextMenu.vue';
 import TagService from '@/api/module/tag.js';
 import { SHOW_SNACKBAR } from '@/store/share/mutationTypes.js';
 import { MESSAGES } from '@/utils/constants.js';
 import { mapActions, mapMutations } from 'vuex';
 import { FETCH_CATEGORIES } from '@/store/share/actionTypes.js';
+import ContextMenu from '@/views/header/component/category/ContextMenu.vue';
+import CategoryEditModal from '@/views/header/component/category/CategoryEditModal.vue';
 
 export default {
   name: 'TagContextMenu',
   components: {
     ContextMenu,
+    CategoryEditModal,
   },
   props: {
     tag: {
@@ -35,9 +40,10 @@ export default {
   data() {
     return {
       items: [
-        { title: '카테고리 변경', icon: 'mdi-pencil', action: '' },
+        { title: '카테고리 변경', icon: 'mdi-pencil', action: this.openEditDialog },
         { title: '태그 삭제', icon: 'mdi-delete', action: this.onDeleteTag },
       ],
+      editCategoryDialog: false,
     };
   },
   methods: {
@@ -51,6 +57,9 @@ export default {
       } catch {
         this[SHOW_SNACKBAR](MESSAGES.TAG.DELETE.FAIL);
       }
+    },
+    openEditDialog() {
+      this.editCategoryDialog = true;
     },
   },
 };
