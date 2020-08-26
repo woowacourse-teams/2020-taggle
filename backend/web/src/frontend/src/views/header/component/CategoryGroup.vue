dbde8eb7c5daab951a51663d1b37980266c60893
taejinseok <stj930@gmail.com>
2020-08-26 15:28:45 +0900

    feat(frontend): category context menu edit 기능 api 연동
    - CategoryGroup.vue에서 line 4 hidden 타입 input 태그를 추가했다.
    - line5의 activator 슬롯 과 같은 depth에서 아무 태그가 없으면, 카테고리 목록 Fetch 할때마다 카테고리 타이틀, 컨텍스트 메뉴 아이콘이 사라지는 이슈 발생. input 태그 추가로 해당 이슈 임시 해결

--- backend/web/src/frontend/src/views/header/component/CategoryGroup.vue
+++ backend/web/src/frontend/src/views/header/component/CategoryGroup.vue
@@ -1,11 +1,16 @@
 <template>
   <v-list-group sub-group class="text-left" value="true">
+    <!-- 어떤 식으로든 이 위치에 태그가 없으면 Category 목록 FETCH 할때 제목, 메뉴버튼 컴포넌트가 사라진다.   -->
+    <input type="hidden" />
     <template v-slot:activator>
-      <v-list-item-title class="font-weight-black text-h7">
+      <v-list-item-title v-if="isEditMode">
+        <v-text-field v-model="title" @click.stop="" @keydown.enter.stop="onEditCategory" @keydown.esc="cancelEdit" />
+      </v-list-item-title>
+      <v-list-item-title v-else class="font-weight-black text-h7">
         {{ category.title }}
       </v-list-item-title>
       <v-list-item-action v-if="category.id">
-        <CategoryContextMenu :category="category" />
+        <CategoryContextMenu :category="category" @edit-category="isEditMode = !isEditMode" />
       </v-list-item-action>
     </template>
     <v-list-item v-for="{ id, name } in category.tags" :key="id" :to="{ name: 'bookmarks', params: { id } }">
@@ -25,6 +30,10 @@
 <script>
 import CategoryContextMenu from '@/views/header/component/category/CategoryContextMenu.vue';
 import TagContextMenu from '@/views/header/component/category/TagContextMenu.vue';
+import { EDIT_CATEGORY, FETCH_CATEGORIES } from '@/store/share/actionTypes.js';
+import { mapActions, mapMutations } from 'vuex';
+import { SHOW_SNACKBAR } from '@/store/share/mutationTypes.js';
+import { MESSAGES } from '@/utils/constants.js';
 
 export default {
   name: 'CategoryGroup',
@@ -36,6 +45,30 @@ export default {
     category: {
       type: Object,
       required: true,
+    },
+  },
+  data() {
+    return {
+      isEditMode: false,
+      title: this.category.title,
+    };
+  },
+  methods: {
+    ...mapActions([EDIT_CATEGORY, FETCH_CATEGORIES]),
+    ...mapMutations([SHOW_SNACKBAR]),
+    async onEditCategory() {
+      try {
+        await this[EDIT_CATEGORY]({ id: this.category.id, title: this.title });
+        await this[FETCH_CATEGORIES]();
+        this[SHOW_SNACKBAR](MESSAGES.CATEGORY.EDIT.SUCCESS);
+      } catch (e) {
+        this[SHOW_SNACKBAR](MESSAGES.CATEGORY.EDIT.FAIL);
+      }
+      this.cancelEdit();
+    },
+    cancelEdit() {
+      this.isEditMode = false;
+      this.title = this.category.title;
     },
   },
 };
