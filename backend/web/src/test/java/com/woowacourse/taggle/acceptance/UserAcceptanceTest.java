1288b27779922f05637f0856ac0c108b2b07af97
kimjunseok <jnunseok@gmail.com>
2020-08-26 20:55:18 +0900

    feat(backend): user 인수 테스트 수정

--- backend/web/src/test/java/com/woowacourse/taggle/acceptance/UserAcceptanceTest.java
+++ backend/web/src/test/java/com/woowacourse/taggle/acceptance/UserAcceptanceTest.java
@@ -2,15 +2,10 @@ package com.woowacourse.taggle.acceptance;
 
 import static org.assertj.core.api.Assertions.*;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.woowacourse.taggle.user.dto.SessionUser;
-import io.restassured.module.mockmvc.response.MockMvcResponse;
-import io.restassured.response.ExtractableResponse;
 
 public class UserAcceptanceTest extends AcceptanceTest {
 
@@ -30,27 +25,5 @@ public class UserAcceptanceTest extends AcceptanceTest {
 
     private SessionUser getUserInfo() {
         return get("/api/v1/users/user-info", SessionUser.class);
-    }
-
-    private void updateNotificationEmail(final String email) {
-        final Map<String, Object> request = new HashMap<>();
-        request.put("notificationEmail", email);
-
-        put("/api/v1/users/notification-email", request);
-    }
-
-    private void updateNotificationEnabled(final Boolean enabled) {
-        final Map<String, Object> request = new HashMap<>();
-        request.put("notificationEnabled", enabled);
-
-        put("/api/v1/users/notification-enabled", request);
-    }
-
-    private void removeUser() {
-        delete("/api/v1/users");
-    }
-
-    public ExtractableResponse<MockMvcResponse> getUserInfoExtractableResponse() {
-        return getExtractableResponse("/api/v1/users/user-info");
     }
 }
