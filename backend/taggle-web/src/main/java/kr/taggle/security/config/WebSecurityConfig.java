17a753a78e88728399888b274a12bc8948218361
SeYun <ksy90101@naver.com>
2020-09-20 21:52:19 +0900

    refactor(oauth2): provider를 이용해 oauth2 로그인 값 가져오도록 수정

--- backend/taggle-web/src/main/java/kr/taggle/security/config/WebSecurityConfig.java
+++ backend/taggle-web/src/main/java/kr/taggle/security/config/WebSecurityConfig.java
@@ -1,5 +1,10 @@
 package kr.taggle.security.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
@@ -7,10 +12,15 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import kr.taggle.security.provider.CustomOAuth2Provider;
 import kr.taggle.security.service.CustomOAuth2UserService;
 import lombok.RequiredArgsConstructor;
 
@@ -63,5 +73,33 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         corsConfiguration.addExposedHeader("Location");
         source.registerCorsConfiguration("/**", corsConfiguration);
         return source;
+    }
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository(
+            final OAuth2ClientProperties oAuth2ClientProperties,
+            @Value("${spring.security.oauth2.client.registration.kakao.client-id}")final String kakaoClientId,
+            @Value("${spring.security.oauth2.client.registration.kakao.client-secret}")final String kakaoSecretId){
+
+        final List<ClientRegistration> registrations = new ArrayList<>();
+
+        final OAuth2ClientProperties.Registration registration = oAuth2ClientProperties.getRegistration().get("google");
+
+        registrations.add(
+                CommonOAuth2Provider.GOOGLE.getBuilder("google")
+                .clientId(registration.getClientId())
+                .clientSecret(registration.getClientSecret())
+                .scope("email", "profile")
+                .build()
+        );
+
+        registrations.add(
+                CustomOAuth2Provider.KAKAO.getBuilder("kakao")
+                .clientId(kakaoClientId)
+                .clientSecret(kakaoSecretId)
+                .build()
+        );
+
+        return new InMemoryClientRegistrationRepository(registrations);
     }
 }
