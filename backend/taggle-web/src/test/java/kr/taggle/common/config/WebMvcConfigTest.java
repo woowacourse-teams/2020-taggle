151b85fb912c482021855fa6ad7338df95182f27
Ramen6315 <ramen6315@gmail.com>
2020-10-06 18:28:34 +0900

    feat(backend): Etag 미변경시 304 status 반환 테스트 작성

--- backend/taggle-web/src/test/java/kr/taggle/common/config/WebMvcConfigTest.java
+++ backend/taggle-web/src/test/java/kr/taggle/common/config/WebMvcConfigTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
@@ -33,8 +34,29 @@ public class WebMvcConfigTest {
 
         mockMvc.perform(get(uri).accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
+                .andExpect(status().isOk())
                 .andExpect(header().exists("Etag"))
                 .andExpect(header().string("Cache-Control","no-cache, must-revalidate"));
+    }
+
+    @DisplayName("Etag가 변경되지 않으면 304 status를 반환한다")
+    @Test
+    void test2() throws Exception{
+        String uri = "/img/google-button.57a6f216.png";
+
+        MvcResult mvcResult = mockMvc.perform(get(uri))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(header().exists("Etag"))
+                .andReturn();
+
+        String etag = mvcResult.getResponse().getHeader("Etag");
+
+        mockMvc.perform(get(uri).header("If-None-Match", etag))
+                .andDo(print())
+                .andExpect(status().isNotModified())
+                .andExpect(header().exists("Etag"))
+                .andReturn();
     }
 
     @DisplayName("Etag가 다를경우 200 status를 반환한다.")
@@ -42,10 +64,16 @@ public class WebMvcConfigTest {
     void test1() throws Exception {
         String uri = "/img/google-button.57a6f216.png";
 
-        mockMvc.perform(get(uri).header("If-None-Match", "differentEtag"))
+        mockMvc.perform(get(uri))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(header().exists("ETag"))
+                .andExpect(header().exists("Cache-Control"))
+                .andReturn();
+
+        mockMvc.perform(get(uri).header("If-None-Match","anotherEtag"))
+                .andDo(print())
+                .andExpect(status().isOk())
                 .andReturn();
     }
 }
