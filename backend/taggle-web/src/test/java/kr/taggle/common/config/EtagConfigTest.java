e7c4c21eafbff32d1a3394a65884253e17fbe94c
Ramen6315 <ramen6315@gmail.com>
2020-10-08 15:41:30 +0900

    refactor(backend): EtagConfigTest @AutoConfigureMockMvc 추가 및 @BeforeEach, setup 삭제

--- backend/taggle-web/src/test/java/kr/taggle/common/config/EtagConfigTest.java
+++ backend/taggle-web/src/test/java/kr/taggle/common/config/EtagConfigTest.java
@@ -4,29 +4,22 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 public class EtagConfigTest {
 
+    @Autowired
     private MockMvc mockMvc;
-
-    @BeforeEach
-    void setUp(final WebApplicationContext webApplicationContext) {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
-                .addFilter(new ShallowEtagHeaderFilter())
-                .build();
-    }
 
     @DisplayName("getEtagAndCacheControl: 정적 리소스 요청에 Etag가 설정 되고 no-cache, must-revalidate로 Cache-Control 설정된다.")
     @Test
