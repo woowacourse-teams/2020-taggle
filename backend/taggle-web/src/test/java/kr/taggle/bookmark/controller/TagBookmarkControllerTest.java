baa51bddef0779662e30b14cb844c8e822cf500d
Ramen6315 <ramen6315@gmail.com>
2020-10-28 15:31:49 +0900

    refactor(backend): TagBookmarkControllerTest mock 처리 변경

--- backend/taggle-web/src/test/java/kr/taggle/bookmark/controller/TagBookmarkControllerTest.java
+++ backend/taggle-web/src/test/java/kr/taggle/bookmark/controller/TagBookmarkControllerTest.java
@@ -3,57 +3,40 @@ package kr.taggle.bookmark.controller;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import kr.taggle.ControllerTest;
 import kr.taggle.bookmark.docs.TagBookmarkDocumentation;
-import kr.taggle.bookmark.domain.Bookmark;
-import kr.taggle.setup.domain.BookmarkSetup;
-import kr.taggle.setup.domain.TagBookmarkSetup;
-import kr.taggle.setup.domain.TagSetup;
-import kr.taggle.setup.domain.UserSetup;
-import kr.taggle.tag.domain.Tag;
+import kr.taggle.bookmark.service.TagBookmarkService;
+import kr.taggle.user.domain.Role;
 import kr.taggle.user.domain.User;
 
 class TagBookmarkControllerTest extends ControllerTest {
 
-    @Autowired
-    private TagBookmarkSetup tagBookmarkSetup;
-
-    @Autowired
-    private TagSetup tagSetup;
-
-    @Autowired
-    private BookmarkSetup bookmarkSetup;
-
-    @Autowired
-    private UserSetup userSetup;
+    @MockBean
+    TagBookmarkService tagBookmarkService;
 
     private User user;
 
     @BeforeEach
     void setUp() {
-        user = userSetup.save();
+        user = new User(1L, "User","User@gmail.com","notiUser@gamil.com", null, "picture", Boolean.FALSE, Role.USER);
     }
 
     @DisplayName("createTagBookmark: 태그에 하나의 북마크를 추가한다.")
     @Test
     void createTagBookmark() throws Exception {
-        final Tag tag = tagSetup.save(user);
-        final Bookmark bookmark = bookmarkSetup.save(user);
 
-        createByPathVariables(user, "/api/v1/bookmarks/{bookmarkId}/tags/{tagId}", bookmark.getId(), tag.getId())
+        createByPathVariables(user, "/api/v1/bookmarks/{bookmarkId}/tags/{tagId}", 1L, 1L)
                 .andDo(TagBookmarkDocumentation.createTagBookmark());
     }
-
+    //
     @DisplayName("removeTagBookmark: 태그에 속해있는 하나의 북마크를 삭제한다.")
     @Test
     void removeTagBookmark() throws Exception {
-        final Tag tag = tagSetup.save(user);
-        final Bookmark bookmark = bookmarkSetup.save(user);
-        tagBookmarkSetup.save(tag, bookmark);
+        User user = new User(1L, "User","User@gmail.com","notiUser@gamil.com", null, "picture", Boolean.FALSE, Role.USER);
 
-        removeByPathVariables(user, "/api/v1/bookmarks/{bookmarkId}/tags/{tagId}", bookmark.getId(), tag.getId())
+        removeByPathVariables(user, "/api/v1/bookmarks/{bookmarkId}/tags/{tagId}", 1L,1L)
                 .andDo(TagBookmarkDocumentation.removeTagBookmark());
     }
 }
